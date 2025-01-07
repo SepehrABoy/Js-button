5b7a6a90fab979003d41943474da5a39231220aa
SepehrABoy <sepe2581her@gmail.com>
2025-01-07 12:41:56 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function Des() {
+    document.getElementById('BTN').style.transform='translateX(-4px)'
+    document.getElementById('BTN').style.width='93px'
+}
+function Gal() {
+    document.getElementById('BTN').style.transform='translateX(90px)'
+    document.getElementById('BTN').style.width='60px'
+}
+function Cha() {
+    document.getElementById('BTN').style.transform='translateX(152px)'
+    document.getElementById('BTN').style.width='87px'
+}
+function Ver() {
+    document.getElementById('BTN').style.transform='translateX(239px)'
+    document.getElementById('BTN').style.width='72px'
+}
